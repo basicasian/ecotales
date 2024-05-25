2fb1b1e068d9661b533f27652a334c0e0f7d02d1
basicasian <nana.zhang.00@gmail.com>
2024-05-25 18:55:26 +0800

    added format

--- src/app/contribute-panda/contribute-panda.page.ts
+++ src/app/contribute-panda/contribute-panda.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InfiniteScrollCustomEvent } from '@ionic/angular';
 
 @Component({
   selector: 'app-contribute-panda',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContributePandaPage implements OnInit {
 
+  items = [];
   constructor() { }
 
   ngOnInit() {
